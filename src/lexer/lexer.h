3b0e26fe61e5c9aeec68e1d4078fa6d8e166295e
Lukas Anstett <s8luanst@stud.uni-saarland.de>
2022-11-01 11:21:16 +0100

    Use references in lexer

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -9,17 +9,17 @@
 #include "stream.h"
 
 class Lexer {
-    public:
-    Lexer(std::string filename, SymbolInternalizer internalizer)
+   public:
+    Lexer(std::string const& filename, SymbolInternalizer& internalizer)
         : m_stream(LocatableStream(filename))
         , m_internalizer(internalizer) {}
 
     Token next();
 
-    private:
+   private:
     Token readStringLiteral();
     void fail(std::string message);
     
     LocatableStream m_stream;
-    SymbolInternalizer m_internalizer;
+    SymbolInternalizer& m_internalizer;
 };
