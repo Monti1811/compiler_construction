1720514f065d6ce058153164316cca19f813f767
Lukas Anstett <s8luanst@stud.uni-saarland.de>
2022-11-11 12:29:40 +0100

    Setup infrastructure for parser and formatter tests

--- test.py
+++ test.py
@@ -49,38 +49,16 @@ def make():
     except subprocess.CalledProcessError as e:
         print(e.stdout, e.stderr)
 
-def runTokenize(file: str) -> "Tuple[list[str], list[str], int]":
+def runCompiler(arg: str, file: str) -> "Tuple[list[str], list[str], int]":
     try:
-        result = subprocess.run([f"{COMPILER_PATH}", "--tokenize", f"{file}.c4"], capture_output=True, text=True, encoding="ISO-8859-1")
+        result = subprocess.run([f"{COMPILER_PATH}", arg, f"{file}.c4"], capture_output=True, text=True, encoding="ISO-8859-1")
         return result.stdout.splitlines(), result.stderr.splitlines(), result.returncode
     except subprocess.CalledProcessError as e:
         return e.stdout.splitlines(), e.stderr.splitlines(), e.returncode
 
-def expectedTokenize(file: str, expectedFile: str) -> "Tuple[list[str], list[str], int]":
-    expectedLines = open(expectedFile, encoding="ISO-8859-1").read()
-    expectedLines = expectedLines.splitlines()
-    expectedLines = map(lambda line: line.strip(), expectedLines)
-    expectedLines = filter(lambda line: len(line) > 0, expectedLines)
-    expectedLines = map(lambda line: f"{file}.c4:{line}", expectedLines)
-
-    def isErrorLine(line: str) -> bool:
-        return not (re.match(r"^([^:]+:\d+:\d+: error).*?$", line) == None)
-
-    expectedStdout = list()
-    expectedStderr = list()
-
-    for line in expectedLines:
-        (expectedStderr if isErrorLine(line) else expectedStdout).append(line)
-
-    return expectedStdout, expectedStderr, 0 if len(expectedStderr) == 0 else 1
-
-def runTokenizeTest(file: str) -> "None | str":
-    expectedFile: str = f"{file}.tokenized"
-    if not os.path.exists(expectedFile):
-        return None
-
-    expectedStdout, expectedStderr, expectedSuccess = expectedTokenize(file, expectedFile)
-    stdout, stderr, success = runTokenize(file)
+def compareResults(expected: "Tuple[list[str], list[str], int]", actual: "Tuple[list[str], list[str], int]") -> str:
+    expectedStdout, expectedStderr, expectedSuccess = expected
+    stdout, stderr, success = actual
 
     result: list[str] = list()
 
@@ -103,6 +81,56 @@ def runTokenizeTest(file: str) -> "None | str":
 
     return "\n".join(result)
 
+def formatExpected(file: str, expectedFile: str) -> "Tuple[list[str], list[str], int]":
+    expectedLines = open(expectedFile, encoding="ISO-8859-1").read()
+    expectedLines = expectedLines.splitlines()
+    expectedLines = map(lambda line: line.strip(), expectedLines)
+    expectedLines = filter(lambda line: len(line) > 0, expectedLines)
+    expectedLines = map(lambda line: f"{file}.c4:{line}", expectedLines)
+
+    def isErrorLine(line: str) -> bool:
+        return not (re.match(r"^([^:]+:\d+:\d+: error).*?$", line) == None)
+
+    expectedStdout = list()
+    expectedStderr = list()
+
+    for line in expectedLines:
+        (expectedStderr if isErrorLine(line) else expectedStdout).append(line)
+
+    return expectedStdout, expectedStderr, 0 if len(expectedStderr) == 0 else 1
+
+def runTokenizeTest(file: str) -> "None | str":
+    expectedFile: str = f"{file}.tokenized"
+    if not os.path.exists(expectedFile):
+        return None
+
+    expected = formatExpected(file, expectedFile)
+    actual = runCompiler("--tokenize", file)
+
+    return compareResults(expected, actual)
+
+def runParseTest(file: str) -> "None | str":
+    expectedFile: str = f"{file}.parsed"
+
+    expected = ["", "", 0]
+    if os.path.exists(expectedFile):
+        expected = formatExpected(file, expectedFile)
+    
+    actual = runCompiler("--parse", file)
+
+    return compareResults(expected, actual)
+
+def runFormatTest(file: str) -> "None | str":
+    expectedFile: str = f"{file}.formatted"
+    if not os.path.exists(expectedFile):
+        return None
+    
+    expectedStdout = open(expectedFile, encoding="ISO-8859-1").read().splitlines()
+    expected = [expectedStdout, "", 0]
+    actual = runCompiler("--print-ast", file)
+
+    return compareResults(expected, actual)
+
 successCount, failedCount, skippedCount = 0, 0, 0
 
 def runTest(file: str):
@@ -111,14 +139,26 @@ def runTest(file: str):
     print(f"\033[94m{file[6:]}\033[90m ... \033[0m", end = "")
 
     tokenizeResult: "None | str" = runTokenizeTest(file)
+    parseResult: "None | str" = runParseTest(file)
+    formatResult: "None | str" = runFormatTest(file)
 
-    if tokenizeResult == None:
+    if tokenizeResult == None and parseResult == None and formatResult == None:
         print("\033[93mSKIPPED\033[0m")
         skippedCount += 1
-    elif tokenizeResult != "":
+    elif tokenizeResult != None and tokenizeResult != "":
         print("\033[91mFAILED (tokenize)\033[0m")
         if verbose or fullDiff:
             print(tokenizeResult)
+        failedCount += 1
+    elif parseResult != None and parseResult != "":
+        print("\033[91mFAILED (parse)\033[0m")
+        if verbose or fullDiff:
+            print(parseResult)
+        failedCount += 1
+    elif formatResult != None and formatResult != "":
+        print("\033[91mFAILED (format)\033[0m")
+        if verbose or fullDiff:
+            print(parseResult)
         failedCount += 1
     else:
         print("\033[92mOK\033[0m")
